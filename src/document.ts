8cc7a1f278843e6d8e6e9fe3ae2c1978523ffd04
Henrik Hall <henrik.hall@sony.com>
2021-05-26 15:11:28 +0200

    Fix import typo

--- src/document.ts
+++ src/document.ts
@@ -1,6 +1,7 @@
 import {dereference} from 'json-schema-ref-parser';
-import {Deref} from 'src/types';
 import {OpenAPIV3} from 'openapi-types';
+
+import {Deref} from './types';
 
 export async function parseDocument(path: string): Promise<Deref<OpenAPIV3.Document>> {
   return await dereference(path) as Deref<OpenAPIV3.Document>;
